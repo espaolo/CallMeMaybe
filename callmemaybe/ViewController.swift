1b91663f1b3a774fe8161961e0769be373d4fcb3
Paolo Esposito <paolo.esposito@kalpa.it>
2020-09-05 23:31:57 +0200

    Project init, iOS 9 and test target

---
+++ callmemaybe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  callmemaybe
+//
+//  Created by Paolo Esposito on 05/09/2020.
+//  Copyright © 2020 Paolo Esposito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
